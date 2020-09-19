d080fcbae372a772191513a801016b08c9ff138b
eric wang <eric@canva.com>
2020-09-19 15:56:34 +1000

    Add prettier

--- src/process-file/process-file.ts
+++ src/process-file/process-file.ts
@@ -1,11 +1,14 @@
 import { readFileSync as _readFileSync } from 'fs';
 import { findSourceMap } from './find-sourcemap';
 
-export function processFile(filePath: string, {
-  readFileSync = _readFileSync
-}: {
-  readFileSync?: typeof _readFileSync
-} = {}) {
+export function processFile(
+  filePath: string,
+  {
+    readFileSync = _readFileSync,
+  }: {
+    readFileSync?: typeof _readFileSync;
+  } = {},
+) {
   const content = readFileSync(filePath, { encoding: 'utf-8' });
   const sourcemapPath = findSourceMap(content);
 }
