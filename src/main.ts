61d3761a8526498f31c45bb8828abc0ff8250e34
Eric Wang <wjun0912@gmail.com>
2020-09-19 15:57:16 +1000

    Add prettier (#3)

--- src/main.ts
+++ src/main.ts
@@ -5,34 +5,34 @@ import { processFile } from './process-file/process-file';
 
 type CliArgs = {
   files: readonly string[];
-}
+};
 
 function reportInvalidFiles(files: readonly string[]) {
-  const nonExistingFiles = files.filter(file => !existsSync(file));
-  throw new Error([
-    'the following files do not exists',
-    ...nonExistingFiles,
-  ].join(EOL));
+  const nonExistingFiles = files.filter((file) => !existsSync(file));
+  throw new Error(
+    ['the following files do not exists', ...nonExistingFiles].join(EOL),
+  );
 }
 
 function handler({ files }: CliArgs) {
   reportInvalidFiles(files);
-  files.forEach(file => processFile(file));
+  files.forEach((file) => processFile(file));
 }
 
 export function main() {
   Yargs.command('$0 <files...>', 'generate css classname mapping', {
-    builder: (): Yargs.Argv<CliArgs> => Yargs.positional('files', {
-      describe: 'path to css files',
-      demandOption: true,
-      type: 'string'
-    }) as any,
+    builder: (): Yargs.Argv<CliArgs> =>
+      Yargs.positional('files', {
+        describe: 'path to css files',
+        demandOption: true,
+        type: 'string',
+      }) as any,
     handler,
   })
-  .strict(true)
-  .exitProcess(true)
-  .demandCommand()
-  .showHelpOnFail(false, 'Specify --help for available options')
-  .help()
-  .parse();
+    .strict(true)
+    .exitProcess(true)
+    .demandCommand()
+    .showHelpOnFail(false, 'Specify --help for available options')
+    .help()
+    .parse();
 }
