eb5a38d3fd092c6422f6f8308c00bd104f8d9532
Eric Wang <wjun0912@gmail.com>
2020-09-19 16:56:14 +1000

    Read sourcemap content (#7)

--- src/tests/process-file.tests.ts
+++ src/tests/process-file.tests.ts
@@ -1,15 +1,53 @@
 import { EOL } from 'os';
-import { findSourceMap } from '../process-file';
+import { processFile, findSourceMapPath } from '../process-file';
 
-describe('findSourceMap', () => {
-  it('should find the source mapping url in css file', () => {
+describe('findSourceMapPath', () => {
+  it('can find the source mapping url in css file', () => {
     const content = [
       '[dir] .HHNCVg{padding:8px}',
       '/*# sourceMappingURL=2f8c03e21afbe2d0c288.2.css.map*/',
     ].join(EOL);
-    const sourcemapPath = findSourceMap(content).expect(
+    const sourcemapPath = findSourceMapPath(content).expect(
       'unable to find sourcemap url',
     );
     expect(sourcemapPath).toEqual('2f8c03e21afbe2d0c288.2.css.map');
+  });
+});
+
+describe('processFile', () => {
+  const existsSync = jest.fn();
+  const readFileSync = jest.fn();
+
+  beforeEach(() => {
+    existsSync.mockRestore();
+    readFileSync.mockRestore();
+  });
+
+  it('can read the sourcemap content based on css file', () => {
+    const cssFilePath = 'a.css';
+    const sourcemapPath = '2f8c03e21afbe2d0c288.2.css.map';
+    const cssContent = [
+      '[dir] .HHNCVg{padding:8px}',
+      '/*# sourceMappingURL=2f8c03e21afbe2d0c288.2.css.map*/',
+    ].join(EOL);
+    const actualSourcemapContent = 'hello world';
+
+    readFileSync
+      .mockReturnValueOnce(cssContent)
+      .mockReturnValueOnce(actualSourcemapContent);
+    existsSync.mockReturnValue(true);
+
+    const gotSourcemapContent = processFile(cssFilePath, {
+      existsSync,
+      readFileSync,
+    }).unwrap();
+
+    expect(gotSourcemapContent).toEqual(actualSourcemapContent);
+    expect(readFileSync.mock.calls).toEqual([
+      [cssFilePath, { encoding: 'utf-8' }],
+      [sourcemapPath, { encoding: 'utf-8' }],
+    ]);
+    expect(existsSync).toHaveBeenCalledTimes(1);
+    expect(existsSync).toHaveBeenCalledWith(sourcemapPath);
   });
 });
