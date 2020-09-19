eac3b507404ad317f399b477bee334046601c714
eric wang <eric@canva.com>
2020-09-19 16:55:23 +1000

    Read sourcemap content

--- src/process-file.ts
+++ src/process-file.ts
@@ -1,7 +1,12 @@
-import { readFileSync as _readFileSync } from 'fs';
+import { existsSync as _existsSync, readFileSync as _readFileSync } from 'fs';
 import { Ok, Err, Result } from 'ts-results';
 
-export function findSourceMap(content: string): Result<string, null> {
+type FsStub = {
+  readFileSync?: typeof _readFileSync;
+  existsSync?: typeof _existsSync;
+};
+
+export function findSourceMapPath(content: string): Result<string, null> {
   const regex = /^\/\*# sourceMappingURL=(.+)\*\//m;
   const match = regex.exec(content);
   if (match == null || match.length !== 2) {
@@ -12,13 +17,17 @@ export function findSourceMap(content: string): Result<string, null> {
 
 export function processFile(
   filePath: string,
-  {
-    readFileSync = _readFileSync,
-  }: {
-    readFileSync?: typeof _readFileSync;
-  } = {},
-): string {
+  { readFileSync = _readFileSync, existsSync = _existsSync }: FsStub = {},
+): Result<string, string> {
   const content = readFileSync(filePath, { encoding: 'utf-8' });
-  const sourcemapPathResult = findSourceMap(content);
-  return sourcemapPathResult.expect(`Fail to find sourcemap in ${filePath}`);
+  const sourcemapPathResult = findSourceMapPath(content);
+  if (sourcemapPathResult.err) {
+    return Err(`Fail to find sourcemap in ${filePath}`);
+  }
+  const sourcemapPath = sourcemapPathResult.val;
+  if (!existsSync(sourcemapPath)) {
+    return Err(`sourcemap@${filePath} does not exist`);
+  }
+  const sourcemapContent = readFileSync(sourcemapPath, { encoding: 'utf-8' });
+  return Ok(sourcemapContent);
 }
