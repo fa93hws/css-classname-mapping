5aac0b99d83464716fd512cf0db8910c01e4676e
Eric Wang <wjun0912@gmail.com>
2020-10-04 20:22:20 +1100

    write output to file (#17)

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,27 @@
 import * as Yargs from 'yargs';
+import { writeFileSync } from 'fs';
+
 import { FindSourcePositionModule } from './debug/find-source-position';
 import { FindOriginalNameModule } from './debug/find-original-classname';
+import { ClassnameMapping } from './processor/classname-mapping';
 import { processFile } from './processor/process-file';
 
 type CliArgs = {
   files: readonly string[];
+  output?: string;
 };
+
+async function handler({ files, output }: CliArgs) {
+  const classnameMapping = new ClassnameMapping();
+  await Promise.all(files.map((file) => processFile(file, classnameMapping)));
+  const mappingObject = classnameMapping.toOutputObject();
+  const outputStr = JSON.stringify(mappingObject, null, 2);
+  if (output != null) {
+    writeFileSync(output, outputStr);
+  } else {
+    console.log(outputStr); // eslint-disable-line no-console
+  }
+}
 
 export function main(): void {
   Yargs.command('$0 <files...>', 'generate css classname mapping', {
@@ -16,8 +32,11 @@ export function main(): void {
         type: 'string',
         // see https://github.com/yargs/yargs/issues/1392
         // eslint-disable-next-line @typescript-eslint/no-explicit-any
+      }).option('output', {
+        describe: 'output path, print to console if omitted',
+        type: 'string',
       }) as any,
-    handler: ({ files }: CliArgs) => files.forEach(processFile),
+    handler,
   })
     .command(FindSourcePositionModule)
     .command(FindOriginalNameModule)
